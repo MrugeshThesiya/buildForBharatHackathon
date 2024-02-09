c4d731e4071257b94912cbb216b61bf8d7abfd78
Twinkal Ajmera <ajmeratwinkal1728@gmail.com>
2024-02-09 18:50:52 +0530

    Navbar fixed

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import codeKalakaarLogo from "/images/DigiCatLogo.jpg";
 import { useAuth0 } from "@auth0/auth0-react";
+import { Menu, X } from 'lucide-react'
 
 function Navbar() {
   const navigate = useNavigate();
